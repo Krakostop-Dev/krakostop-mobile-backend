eb0f29863d57117d2bfa4f020e3da63436d97041
Grzegorz Tłuszcz <gtluszcz@gmail.com>
2019-08-24 17:20:42 +0200

    Add protected controller

---
+++ app/controllers/protected_controller.rb
@@ -0,0 +1,33 @@
+class ProtectedController < ApplicationController
+  before_action :authenticate_user
+  helper_method :current_user
+
+  protected
+
+  def current_user
+    @current_user
+  end
+
+  private
+
+  def authenticate_user
+    @current_user = User.find(user_id)
+  rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+    return render(json: {}, status: :unauthorized)
+  end
+
+  def user_id
+    payload = decrypt_jwt(bearer_token)
+    payload[:user_id]
+  end
+
+  def decrypt_jwt(token)
+    Login::JwtDecrypter.new.call(token)
+  end
+
+  def bearer_token
+    pattern = /^Bearer /
+    header  = request.headers.fetch('Authorization', nil)
+    header.gsub(pattern, '') if header && header.match(pattern)
+  end
+end
